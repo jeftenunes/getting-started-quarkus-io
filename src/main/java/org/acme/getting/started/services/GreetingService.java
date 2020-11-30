0d024220d5ed7b7e850cffb30bbcbcfd2e6fe2b5
Jefte Santos <jeftenunes@hotmail.com>
2020-11-29 23:06:00 -0500

    Initial commit

---
+++ src/main/java/org/acme/getting/started/services/GreetingService.java
@@ -0,0 +1,10 @@
+package org.acme.getting.started.services;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+    public String greeting(String name) {
+        return "hello, " + name;
+    }
+}
